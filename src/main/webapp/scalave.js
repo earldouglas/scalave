226e321ff94b57c1f221a298d9cdc7112a5e3d86
James Earl Douglas <james@earldouglas.com>
2014-06-27 16:27:24 -0700

    Cleanup JS; add rows/cols config

--- src/main/webapp/scalave.js
+++ src/main/webapp/scalave.js
@@ -1,26 +1,13 @@
 (function() {
 
-  window['scalave_println'] = function(elem, out) {
-    var output = elem;
-    while (output.firstChild) { output.removeChild(output.firstChild); }
+  function println(elem, out) {
+    while (elem.firstChild) { elem.removeChild(elem.firstChild); }
     var pre = document.createElement('pre');
-    pre.className = 'scalave_output';
     pre.style.paddingLeft = '12px';
     pre.style.borderLeft = '1px solid #666';
     pre.style.fontSize = '12px';
     pre.innerHTML = out;
-    output.appendChild(pre);
-  };
-
-  var rnd = Math.random().toString(36).substring(2);
-
-  function prefix(x) {
-    return 'scalave_' + rnd + '_' + x;
-  }
-
-  window[prefix('println')] = function(out) {
-    var output = document.getElementById(prefix('output'));
-    scalave_println(output, out);
+    elem.appendChild(pre);
   };
 
   function unindent(x) {
@@ -39,34 +26,24 @@
     return lines.join('\n');
   }
 
+  function getOrElse(x,y) {
+    return x ? x : y;
+  }
+
   function setup() {
 
     var scripts = document.getElementsByTagName('script');
     var script  = scripts[scripts.length - 1];
 
     var src        = document.createElement('textarea');
-    src.id         = prefix('src');
     src.name       = 'src';
-    src.rows       = '8';
-    src.cols       = '72';
+    src.rows       = getOrElse(script.getAttribute('rows'), '8');
+    src.cols       = getOrElse(script.getAttribute('cols'), '72');
     src.style.fontFamily = 'Courier New, monospace';
     src.style.fontSize = '10pt';
     src.value      = unindent(script.text);
 
     var run        = document.createElement('button');
-    run.onclick =
-      function() {
-        var output = document.getElementById(prefix('output'));
-        scalave_println(output, "running...");
-        var head = document.getElementsByTagName('head')[0];
-        var script = document.createElement('script');
-        var src = encodeURIComponent(document.getElementById(prefix('src')).value);
-        var url = '//scalave.earldouglas.com/?jsonp=' + prefix('println') + '&src=' + src;
-        script.setAttribute('src', url);
-        head.appendChild(script);
-        head.removeChild(script);
-      };
-
     run.innerHTML  = 'run';
 
     var input_div1 = document.createElement('div');
@@ -80,12 +57,28 @@
     input.appendChild(input_div2);
 
     var output     = document.createElement('div');
-    output.id      = prefix('output');
     output.style.fontSize = '12px';
     output.innerHTML = '&nbsp;';
 
     script.parentNode.insertBefore(output, script.nextSibling);
     script.parentNode.insertBefore(input, script.nextSibling);
+
+    var callbackName = 'scalave_' + scripts.length;
+    window[callbackName] = function(out) {
+      println(output, out);
+    };
+
+    run.onclick =
+      function() {
+        println(output, "running...");
+        var head = document.getElementsByTagName('head')[0];
+        var script = document.createElement('script');
+        var srcEnc = encodeURIComponent(src.value);
+        var url = '//scalave.earldouglas.com/?jsonp=' + callbackName + '&src=' + srcEnc;
+        script.setAttribute('src', url);
+        head.appendChild(script);
+        head.removeChild(script);
+      };
 
   }
 
